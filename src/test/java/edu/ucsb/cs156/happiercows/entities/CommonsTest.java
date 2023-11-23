b1c7c3b993dd007ce090bf69c526d9763a76a54a
madison <longmadison@gmail.com>
2023-11-22 17:36:00 -0800

    ml- added tests for in progress function

--- src/test/java/edu/ucsb/cs156/happiercows/entities/CommonsTest.java
+++ src/test/java/edu/ucsb/cs156/happiercows/entities/CommonsTest.java
@@ -10,10 +10,10 @@ import java.time.LocalDateTime;
 
 public class CommonsTest {
 
-    LocalDateTime s1 = LocalDateTime.parse("2012-01-10");
-    LocalDateTime s2 = LocalDateTime.parse("2024-01-10");
-    LocalDateTime e1 = LocalDateTime.parse("-01-10");
-    LocalDateTime e2 = LocalDateTime.parse("2023-01-10");
+    LocalDateTime s1 = LocalDateTime.parse("2012-01-10T15:50:10");
+    LocalDateTime s2 = LocalDateTime.parse("2024-01-10T15:50:10");
+    LocalDateTime e1 = LocalDateTime.parse("2030-01-10T15:50:10");
+    LocalDateTime e2 = LocalDateTime.parse("2023-01-10T15:50:10");
     
     @Test
     void test_gameInProgressTrue() throws Exception {
@@ -21,10 +21,10 @@ public class CommonsTest {
     }
     @Test
     void test_gameInProgress_False_Not_Started() throws Exception {
-        assertEquals(false, Commons.builder().startingDate(e1).lastDay(e2).build().gameInProgress());
+        assertEquals(false, Commons.builder().startingDate(s2).lastDay(e1).build().gameInProgress());
     }
     @Test
     void test_gameInProgress_False_Already_Ended() throws Exception {
-        assertEquals(false, Commons.builder().startingDate(e2).lastDay(s2).build().gameInProgress());
+        assertEquals(false, Commons.builder().startingDate(s1).lastDay(e2).build().gameInProgress());
     }
 }
