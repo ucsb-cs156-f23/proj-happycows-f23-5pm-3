b205017175f3375e00cb59ddad58aa7da2c855b6
wyatthspivak <91758578+wyatthspivak@users.noreply.github.com>
2023-11-25 12:50:56 -0800

    Update src/main/java/edu/ucsb/cs156/happiercows/repositories/AnnouncementRepository.java
    Co-authored-by: Phill Conrad <pconrad@cs.ucsb.edu>

--- src/main/java/edu/ucsb/cs156/happiercows/repositories/AnnouncementRepository.java
+++ src/main/java/edu/ucsb/cs156/happiercows/repositories/AnnouncementRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import edu.ucsb.cs156.happiercows.entities.Announcement;
 
+@Repository
 public interface AnnouncementRepository extends CrudRepository<Announcement, Long> {
     
 }
